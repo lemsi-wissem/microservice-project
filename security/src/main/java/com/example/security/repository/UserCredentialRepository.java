d0f9e6fd24b67c3ece8da365d78a9a117c63c6de
Houssem Sakli <sakli.houssem@yahoo.com>
2023-05-02 22:54:32 +0100

    Added security service

---
+++ security/src/main/java/com/example/security/repository/UserCredentialRepository.java
@@ -0,0 +1,11 @@
+package com.example.security.repository;
+
+import com.example.security.entity.UserCredential;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+public interface UserCredentialRepository extends JpaRepository<UserCredential, Integer> {
+    Optional<UserCredential> findByName(String username);
+}
