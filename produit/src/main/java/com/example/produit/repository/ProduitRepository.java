d16a722acf6ffb3e9dff02513af4f78f747973d1
wissemMuller <wissem.muller@finconnect-fs.com>
2023-05-02 22:34:39 +0100

    eureka server, config server, gateway, auth, notification service

--- produit/src/main/java/com/example/produit/repository/ProduitRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.produit.repository;
-
-import com.example.produit.model.Produit;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProduitRepository extends CrudRepository<Produit, Integer> {
-}
